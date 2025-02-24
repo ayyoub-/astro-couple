006e85aab28a82cd94dbdf04b19095e1519b20bb
aem <elmerzougui@zeturf.com>
2025-02-24 17:59:31 +0100

    Ignore .env file

---
+++ ai_client/src/Entity/AstroCouple.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Astro
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $date_naissance = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $lieu_naissance = null;
+
+    // Getters et setters
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $date_naissance): self
+    {
+        $this->date_naissance = $date_naissance;
+        return $this;
+    }
+
+    public function getLieuNaissance(): ?string
+    {
+        return $this->lieu_naissance;
+    }
+
+    public function setLieuNaissance(string $lieu_naissance): self
+    {
+        $this->lieu_naissance = $lieu_naissance;
+        return $this;
+    }
+}
---
+++ ai_client/src/Form/AstroCoupleType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\AstroCouple;
+
+class AstroType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $personIndex = $options['person_index'];
+        $builder
+            ->add('nom1', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('date_naissance1', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date et heure de naissance'
+            ])
+            ->add('lieu_naissance1', TextType::class, [
+                'label' => 'Lieu de naissance'
+            ])
+            ->add('nom2', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('date_naissance2', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date et heure de naissance'
+            ])
+            ->add('lieu_naissance2', TextType::class, [
+                'label' => 'Lieu de naissance'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AstroCouple::class,
+            'person_index' => null,
+        ]);
+    }
+}
